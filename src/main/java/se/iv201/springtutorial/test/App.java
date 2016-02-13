18303a67856e1db7218302895ce3287328931df6
Ali <Ali.joshua.stockholm@gmail.com>
2016-02-13 21:04:20 +0100

    Initial commit

---
+++ src/main/java/se/iv201/springtutorial/test/App.java
@@ -0,0 +1,13 @@
+package se.iv201.springtutorial.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
